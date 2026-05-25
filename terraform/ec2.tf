d057c1f751cf421700c151642fcbe96b34d9fe5b
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-05-25 15:21:31 +0530

    Add multiple AWS EC2 instance resources

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,20 @@
+resource "aws_instance" "frontend" {
+  ami = var.ami
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.public.id
+}
 
+resource "aws_instance" "app" {
+  ami = var.ami
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "middleware" {
+  ami = var.ami
+  instance_type = "t2.micro"
+}
+
+resource "aws_instance" "db" {
+  ami = var.ami
+  instance_type = "t2.micro"
+}
