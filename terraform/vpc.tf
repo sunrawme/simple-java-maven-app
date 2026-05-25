b5923d48879cd1c0510d9a677c23abcf041ceb64
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-05-25 15:29:17 +0530

    Add public subnet resource to VPC configuration

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,3 +6,14 @@ resource "aws_vpc" "main" {
     Name = "demo-vpc"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
