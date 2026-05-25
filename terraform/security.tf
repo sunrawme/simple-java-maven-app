2593c956e25311be394cc090e35edaf089aefa77
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-05-25 15:21:06 +0530

    Add AWS security group for frontend with ingress rules

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,1 +1,17 @@
+resource "aws_security_group" "frontend_sg" {
+  vpc_id = aws_vpc.main.id
 
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
