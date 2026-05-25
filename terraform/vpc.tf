1076cb935e25f8ec32b4bacae2494433279a930f
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-05-25 15:29:43 +0530

    Change public subnet availability zone to us-east-1

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1"
   map_public_ip_on_launch = true
 
   tags = {
