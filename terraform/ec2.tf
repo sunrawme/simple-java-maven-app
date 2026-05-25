da3124d2fc61e835119ab5525a60112adebe1572
sunrawme <71823093+sunrawme@users.noreply.github.com>
2026-05-25 15:30:09 +0530

    Align formatting and add tags for EC2 instances

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,20 +1,36 @@
 resource "aws_instance" "frontend" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.public.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "frontend"
+  }
 }
 
 resource "aws_instance" "app" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "application"
+  }
 }
 
 resource "aws_instance" "middleware" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "middleware"
+  }
 }
 
 resource "aws_instance" "db" {
-  ami = var.ami
+  ami           = var.ami
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "database"
+  }
 }
